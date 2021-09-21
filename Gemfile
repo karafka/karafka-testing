bd1b08e1420fcf469c7edc80e09cb53e1bc4958d
Maciej Mensfeld <maciej@mensfeld.pl>
2021-09-21 12:44:45 +0200

    2.0 sync

--- Gemfile
+++ Gemfile
@@ -4,4 +4,6 @@ source 'https://rubygems.org'
 
 plugin 'diffend'
 
+gem 'karafka', github: 'karafka/karafka', branch: '2.0-wip'
+
 gemspec
---
+++ lib/karafka/testing/rspec/proxy.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+
+module Karafka
+  module Testing
+    module RSpec
+      # Proxy object for a nicer `karafka.` API within RSpec
+      class Proxy
+        # @param rspec_example [RSpec::ExampleGroups] rspec context
+        def initialize(rspec_example)
+          @rspec_example = rspec_example
+        end
+
+        # @args Anything that the `#karafka_consumer_for` accepts
+        def consumer_for(*args)
+          @rspec_example.karafka_consumer_for(*args)
+        end
+
+        # @args Anything that the `#karafka_publish` accepts
+        def publish(*args)
+          @rspec_example.karafka_publish(*args)
+        end
+      end
+    end
+  end
+end
