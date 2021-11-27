1f7461723b9f5e64455180b64e5bc1f51c7ee03a
Maciej Mensfeld <maciej@mensfeld.pl>
2021-11-27 20:22:25 +0100

    #71

---
+++ lib/karafka/testing/dummy_client.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Karafka
+  module Testing
+    # A dummy client that takes over client delegated methods from the consumers
+    # For specs we do not mark anything as consumed, nor do we really send heartbeats.
+    # Those things are tested in the framework itself
+    class DummyClient
+      %i[
+        mark_as_consumed
+        mark_as_consumed!
+      ].each do |caught_delegator|
+        define_method(caught_delegator) { |*| }
+      end
+    end
+  end
+end
