faca64e881d7da6cbd021a8bf68268d18edee487
Maciej Mensfeld <maciej@mensfeld.pl>
2023-06-13 09:51:29 +0200

    remove redundant code

--- lib/karafka/testing/spec_producer_client.rb
+++ lib/karafka/testing/spec_producer_client.rb
@@ -4,39 +4,20 @@ module Karafka
   module Testing
     # Spec producer client used to buffer messages that we send out in specs
     class SpecProducerClient < ::WaterDrop::Clients::Buffered
-      attr_accessor :messages
-
-      # Sync fake response for the message delivery to Kafka, since we do not dispatch anything
-      class SyncResponse
-        # @param _args Handler wait arguments (irrelevant as waiting is fake here)
-        def wait(*_args)
-          false
-        end
-      end
-
       # @param rspec [RSpec::Core::ExampleGroup] rspec example we need to hold to trigger actions
       #   on it that are rspec context aware
       def initialize(rspec)
         super(nil)
         @rspec = rspec
-        self.messages = []
       end
 
       # "Produces" message to Kafka. That is, it acknowledges it locally, adds it to the internal
       # buffer and adds it (if needed) into the current consumer messages buffer
       # @param message [Hash] `Karafka.producer.produce_sync` message hash
       def produce(message)
-        messages << message
-
         @rspec._karafka_add_message_to_consumer_if_needed(message)
 
-        SyncResponse.new
-      end
-
-      # Clears internal buffer
-      # Used in between specs so messages do not leak out
-      def reset
-        messages.clear
+        super
       end
     end
   end
